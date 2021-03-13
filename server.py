a37fd4926fad2c97059088d158d51e5b486d31dc
ning <ning@abc.com>
2021-03-13 16:35:31 +0800

    removed ssl certificates

--- server.py
+++ server.py
@@ -61,4 +61,4 @@ def fail_suffix():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=433, ssl_context=("./ning.crt", "./ning.key"))
+    app.run(debug=True, host='0.0.0.0', port=80)
